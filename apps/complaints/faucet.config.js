0395cb4e81c2052e8816c551ee16ea584388839b
Joy Heron <joy.heron@innoq.com>
2021-08-06 11:55:45 +0200

    Add icons for navbar items

--- apps/complaints/faucet.config.js
+++ apps/complaints/faucet.config.js
@@ -1,9 +1,13 @@
 "use strict";
 
 module.exports = {
-    watchDirs: ["./views/components", "./lib/styles"],
-    sass: [{
-        source: "./lib/styles/index.scss",
-        target: "./dist/bundle.css"
-    }]
+	watchDirs: ["./views/components", "./lib/styles"],
+	sass: [{
+		source: "./lib/styles/index.scss",
+		target: "./dist/bundle.css"
+	}],
+	static: [{
+		source: "./lib/images",
+		target: "./dist/images"
+	}]
 };
