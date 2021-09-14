b1f01490e8f3482a1cf65f129d211a458c56daac
Joy Heron <joy.heron@innoq.com>
2021-09-14 14:43:36 +0200

    Component Library example

--- apps/pattern-library/components/button/index.js
+++ apps/pattern-library/components/button/index.js
@@ -1,14 +1,14 @@
 // /* POSSIBLE SOLUTION */
-// class AlertButton extends HTMLButtonElement {
-// 	connectedCallback() {
-// 		this.addEventListener("click", () => {
-// 			alert(this.text);
-// 		});
-// 	}
+class AlertButton extends HTMLButtonElement {
+	connectedCallback() {
+		this.addEventListener("click", () => {
+			alert(this.text);
+		});
+	}
 
-// 	get text() {
-// 		return this.getAttribute("data-text") || "button clicked!";
-// 	}
-// }
+	get text() {
+		return this.getAttribute("data-text") || "button clicked!";
+	}
+}
 
-// customElements.define("custom-button", AlertButton, { extends: 'button' });
+customElements.define("custom-button", AlertButton, { extends: 'button' });
